cc48500c06dca6477f961ebbf4a3d926d07e33c6
RoboSapiens2021 <88692802+RoboSapiens2021@users.noreply.github.com>
2022-12-31 02:02:17 -0500

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -56,14 +56,17 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
-    // database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
+    redis_url: REDIS_URL,
+//     For more production-like environment install PostgresQL
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+//     database_database: "./medusa-db.sql",
+//     database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
+    jwt_secret: JWT_SECRET,
+    cookie_secret: COOKIE_SECRET,
+
   },
   plugins,
 };
